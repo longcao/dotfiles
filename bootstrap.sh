61885b5493442f76cd79428147ea6979d13a27b9
Long Cao <longcao@gmail.com>
2015-10-22 11:52:37 -0400

    add .psqlrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ git pull
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    for filename in .aliases .bash_profile .bash_prompt .bashrc .ctags .dir_colors .exports .functions .gitconfig .inputrc .vimrc
+    for filename in .aliases .bash_profile .bash_prompt .bashrc .ctags .dir_colors .exports .functions .gitconfig .inputrc .psqlrc .vimrc
         do
             ln -s -f "$(dirname "$0")"/$filename ~
         done
