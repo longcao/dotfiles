9aa1ea036147cbaa788bbecc122d36f4cb91580c
Long Cao <longcao@gmail.com>
2016-03-20 17:44:02 -0400

    add .lein/profiles.clj and link it in bootstrap.sh

---
+++ .lein/profiles.clj
@@ -0,0 +1,3 @@
+{:user {:plugins [[cider/cider-nrepl "0.10.2"]
+                  [venantius/ultra "0.4.1"]
+                  [com.jakemccrary/lein-test-refresh "0.14.0"]]}}
--- bootstrap.sh
+++ bootstrap.sh
@@ -9,10 +9,15 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
             ln -s -f "$(dirname "$0")"/$filename ~
         done
 fi
+
 mkdir -p ~/.vim/tmp
 mkdir -p ~/.vim/colors
 mkdir -p ~/.psql_history
 mkdir -p ~/.sbt/0.13
+mkdir -p ~/.lein
+
 ln -s -f ~/"$(dirname "$0")"/.sbt/0.13/local.sbt ~/.sbt/0.13/local.sbt
+ln -s -f ~/"$(dirname "$0")"/.lein/profiles.clj ~/.lein/profiles.clj
 ln -s -f ~/"$(dirname "$0")"/.vim/colors/molokai.vim ~/.vim/colors/molokai.vim
+
 source "$HOME/.bashrc"
