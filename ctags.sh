7d13faf326ac0df83b8a05bf2e51ca0cea813672
Long Cao <longcao@gmail.com>
2016-10-24 18:09:25 -0400

    ignore more files in ctags

--- ctags.sh
+++ ctags.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # Generates ctags using exuberant-ctags
 if [ ! -d ~/.vim/tags ]; then
-	mkdir ~/.vim/tags
+  mkdir ~/.vim/tags
 fi
 cd ~/projects/
 ctags -f ~/.vim/tags/scala-tags.tag \
-    -h [".scala"] -R \
-    --exclude="target" \
-    --exclude="public" \
-    --exclude="resources" \
-    --exclude="node_modules" \
-    --exclude="scripts" \
-    --exclude="utils" \
-    --exclude="javascripts" \
-    --exclude="*.js" \
-    --exclude="build" \
-    --totals=yes \
-	--tag-relative=yes
+  -h [".scala"] -R \
+  --exclude="target" \
+  --exclude="public" \
+  --exclude="resources" \
+  --exclude="node_modules" \
+  --exclude="scripts" \
+  --exclude="utils" \
+  --exclude="javascripts" \
+  --exclude="*.js" \
+  --exclude="build" \
+  --exclude="*.pem" \
+  --exclude="*.asc" \
+  --totals=yes \
+  --tag-relative=yes
