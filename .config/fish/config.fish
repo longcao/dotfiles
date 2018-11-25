d9c532290ca8b2a89f9b12d962bb8ccad2e9fd96
Long Cao <longcao@gmail.com>
2018-11-25 17:23:47 -0500

    init fish shell config

---
+++ .config/fish/config.fish
@@ -0,0 +1,51 @@
+############### ENVIRONMENT VARIABLES ###############
+# Set PATH
+set -x PATH ~/.cargo/bin $PATH
+set -x PATH ~/.okta/bin $PATH
+
+# Make vim the default editor
+set -x EDITOR vim
+
+# Java options
+set -x JAVA_OPTS "-XX:ReservedCodeCacheSize=256m -XX:MaxMetaspaceSize=512m"
+
+# sbt options since it doesn't pick up JAVA_OPTS
+set -x SBT_OPTS "-XX:ReservedCodeCacheSize=256m -Xmx4096m"
+
+# Choose specific Python version for pyenv
+set -x PYENV_VERSION "3.6.6"
+
+############### ABBREVIATIONS ###############
+# Easier navigation
+abbr -a -- - 'cd -'
+abbr -a ... 'cd ../..'
+
+############### ALIASES ###############
+# OS X has no `md5sum`, so use `md5` as a fallback
+alias md5sum="md5"
+
+# Copy the current directory to clipboard
+alias cpd="pwd | pbcopy"
+
+# grep color
+alias grep="grep --color='auto'"
+
+# warn if overwriting file when using mv
+alias mv="mv -i"
+
+# megaclean (delete target/ folders) all sbt/play projects in a given directory
+alias megaclean="find . -name target | xargs rm -Rf"
+
+# tree ignores target
+alias tree="tree -I 'target|target-*'"
+
+# paste from clipboard and pretty print JSON
+alias pj="pbpaste | jq ."
+alias pastejson="pj"
+
+# use gdate
+alias date='gdate'
+
+############### OTHER INIT ###############
+# Load pyenv automatically
+status --is-interactive; and source (pyenv init -|psub)
