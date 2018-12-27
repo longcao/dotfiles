15bc9a6ef5894d2422f4e4fb48dc4e4405f87d21
Long Cao <longcao@gmail.com>
2018-12-27 15:16:53 -0600

    vscode: add fish syntax highlighting

--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -1,4 +1,5 @@
 code --install-extension nonylene.dark-molokai-theme
 code --install-extension scala-lang.scala
 code --install-extension scalameta.metals
+code --install-extension skyapps.fish-vscode
 code --install-extension vscodevim.vim
