9c0f26003c5b2f76729a2eeb13d535cffeee1fd1
Long Cao <longcao@gmail.com>
2020-07-25 12:17:51 -0500

    fix fmt

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,10 @@ git pull
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-    for filename in .aliases .bash_profile .bash_prompt .ctags .dir_colors .exports .functions .gitconfig .inputrc .psqlrc .vimrc
-        do
-            ln -s -f "$(dirname "$0")"/$filename ~
-        done
+  for filename in .aliases .bash_profile .bash_prompt .ctags .dir_colors .exports .functions .gitconfig .inputrc .psqlrc .vimrc
+  do
+    ln -s -f "$(dirname "$0")"/$filename ~
+  done
 fi
 
 mkdir -p ~/.config/fish
