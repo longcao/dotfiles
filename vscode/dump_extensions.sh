62884755c415353f78ccaccc10c653eaf8c57ef8
Long Cao <longcao@gmail.com>
2020-09-23 12:57:48 -0500

    shebangs

--- vscode/dump_extensions.sh
+++ vscode/dump_extensions.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 code --list-extensions | xargs -L 1 echo code --install-extension > install_extensions.sh
--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 code --install-extension nonylene.dark-molokai-theme
 code --install-extension scala-lang.scala
 code --install-extension scalameta.metals
